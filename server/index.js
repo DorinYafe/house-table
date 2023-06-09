3419eac3ad10280f1c7af15c853be48e67017d7a
Dorin Yafe <dorinyafe@gmail.com>
2023-06-09 17:50:41 +0300

    feat(routes): add routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const app = express();
 
 const db = require('./models');
 
+app.use(express.json());
+
+// Routes
+const housesRouter = require('./routes/houses');
+app.use('/houses', housesRouter);
+
 db.sequelize.sync().then(() => {
   app.listen(3002, () => {
     console.log('Server started on port 3002');
---
+++ server/routes/houses.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const { House } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const houses = await House.findAll();
+    res.json(houses).status(200);
+  } catch (error) {
+    res.status(500);
+    console.error(error);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const house = req.body;
+    await House.create(house);
+    res.json(house).status(201);
+  } catch (error) {
+    res.status(500);
+    console.error(error);
+  }
+});
+
+module.exports = router;
